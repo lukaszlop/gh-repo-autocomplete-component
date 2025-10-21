07109b406a7f3cc2e3f3af77b5910a42820e6762
Łukasz Łopieński <lukaszlop@gmail.com>
2025-10-21 14:10:18 +0200

    chore: initialize project with dependencies and configuration

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+const App = () =>
+    <div className='flex flex-col items-center justify-center h-screen w-full bg-bgContrast'>
+      <h1 className='text-2xl font-bold text-textContrast'>GH Repo Autocomplete Component</h1>
+    </div>
+
+export default App
