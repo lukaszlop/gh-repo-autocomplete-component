d6a572702fde5ddca699ae5d7fa7e36d91f13bd8
Łukasz Łopieński <lukaszlop@gmail.com>
2025-10-26 22:27:16 +0100

    chore(deps): downgrade and cleanup dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () =>
-    <div className='flex flex-col items-center justify-center h-screen w-full bg-bgContrast'>
-      <h1 className='text-2xl font-bold text-textContrast'>GH Repo Autocomplete Component</h1>
+    <div className='flex flex-col items-center justify-center h-screen w-full bg-background'>
+      <h1 className='text-2xl font-bold text-foreground'>GH Repo Autocomplete Component</h1>
     </div>
 
 export default App
